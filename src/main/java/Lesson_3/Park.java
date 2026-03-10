428bcd084abfafa951e79700878d2d43e4dfaf18
KoloskovNV <rgfggw5xst@privaterelay.appleid.com>
2026-03-10 18:25:47 +0400

    Update Park.java
    Измененный вариант

--- src/main/java/Lesson_3/Park.java
+++ src/main/java/Lesson_3/Park.java
@@ -4,29 +4,29 @@ public class Park {
     private String name;
     private String city;
     private Attraction attraction;
-
+    
     public Park(String name, String city) {
         this.name = name;
         this.city = city;
     }
-
+    
     public Attraction getAttraction() {return attraction;}
-
+    
     public void setAttraction(Attraction attraction) {
         this.attraction = attraction;
     }
-
+    
     public class Attraction {
         private String attractionName;
         private String workingHours;
         private int cost;
-
+        
         public Attraction(String attractionName, String workingHours, int cost) {
             this.attractionName = attractionName;
             this.workingHours = workingHours;
             this.cost = cost;
         }
-
+        
         public void printInfo() {
             System.out.println("Аттракцион: " + attractionName);
             System.out.println("Время работы: " + workingHours);
